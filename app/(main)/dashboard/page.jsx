ed8f5856acc144e487e51d9c59ceecf1db9fa910
Harsh Maniar <harshmaniar@Mac.lan>
2025-02-18 16:19:49 +0530

    solved one bug and  preparing for deployment

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,14 +1,13 @@
 import { Suspense } from "react";
 import { getUserAccounts } from "@/actions/dashboard";
 import { getDashboardData } from "@/actions/dashboard";
-// import { CreateAccountDrawer } from "@/components/create-account-drawer";
 import { Card, CardContent } from "@/components/ui/card";
 import { Plus } from "lucide-react";
 import AccountCard from "./_components/AccountCard";
 import BudgetProgress from "./_components/BudgetProgress";
 import DashboardOverview from "./_components/DashboardOverview";
 import { getCurrentBudget } from "@/actions/budgets";
-
+import CreateAccountDrawer from "@/components/CreateAccountDrawer";
 export default async function DashboardPage() {
   const [accounts, transactions] = await Promise.all([
     getUserAccounts(),
@@ -39,14 +38,14 @@ export default async function DashboardPage() {
 
       {/* Accounts Grid */}
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-        {/* <CreateAccountDrawer>
+        <CreateAccountDrawer>
           <Card className="hover:shadow-md transition-shadow cursor-pointer border-dashed">
             <CardContent className="flex flex-col items-center justify-center text-muted-foreground h-full pt-5">
               <Plus className="h-10 w-10 mb-2" />
               <p className="text-sm font-medium">Add New Account</p>
             </CardContent>
           </Card>
-        </CreateAccountDrawer> */}
+        </CreateAccountDrawer>
         {accounts.length > 0 &&
           accounts?.map((account) => (
             <AccountCard key={account.id} account={account} />
