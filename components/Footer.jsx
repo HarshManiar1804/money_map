252aece5e775dd869ef754e6d002a10d314b3bf3
Harsh Maniar <harshmaniar@Mac.lan>
2025-02-18 16:34:24 +0530

    update the footer section

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,10 +1,12 @@
+import Image from "next/image";
 import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-blue-50 py-12">
-      <div className="container max-auto px-4 text-center text-gray-600">
-        <p>MoneyMap By Harsh Maniar</p>
+    <div className="bg-blue-50 p-4 pb-0">
+      <div className="container max-auto px-4 text-center text-gray-600 p-5 flex justify-between items-center">
+        <Image src="/logo.jpeg" width={200} height={60} alt="logo" />
+        <p>Created By Harsh Maniar 👨🏻‍💻</p>
       </div>
     </div>
   );
