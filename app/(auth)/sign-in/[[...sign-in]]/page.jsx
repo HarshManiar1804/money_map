ff7673e27a8bdd57fce80caee2dddbf6efa88523
Harsh Maniar <harshmaniar@Mac.lan>
2025-02-18 17:33:03 +0530

    fixes some bugs

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -2,7 +2,11 @@ import { SignIn } from "@clerk/nextjs";
 import React from "react";
 
 const Page = () => {
-  return <SignIn />;
+  return (
+    <div>
+      <SignIn className="mt-10" />
+    </div>
+  );
 };
 
 export default Page;
--- components/Header.jsx
+++ components/Header.jsx
@@ -8,7 +8,7 @@ import { checkUser } from "@/lib/checkUser";
 const Header = async () => {
   await checkUser();
   return (
-    <div className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
+    <div className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b mb-5">
       <nav className="container mx-auto px-4 py-4  flex items-center justify-between">
         <Link href="/">
           <Image src="/logo.jpeg" width={200} height={60} alt="logo" />
